0e74930f27413a8f079df542ec4f9762cf1d655b
GitHiroki <ringo_hiro@hotmail.co.jp>
2022-05-04 01:39:06 +0900

    webApiのサンプルコードをコミット - クライアント側(apiを呼ぶ側) - サーバサイド側(api側)

---
+++ asp.net/Mvc/WebApi/Client/Controllers/FksmAPIController.cs
@@ -0,0 +1,26 @@
+﻿using MvcMovie.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class FksmAPIController : Controller
+    {
+        // GET: FksmAPI
+        public ActionResult Index()
+        {
+            //iis express
+            //ApiService apiService = new ApiService(@"http://localhost:xxxxx/api/");
+            //ローカルiis
+            ApiService apiService = new ApiService(@"http://xxx.xxx.xxx.xxx:pp/api/");
+            String resBody = apiService.GetRequest("values");
+            ViewBag.resBody = resBody;
+
+
+            return View();
+        }
+    }
+}
---
+++ asp.net/Mvc/WebApi/Client/Services/ApiService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MvcMovie.Services
+{
+    public class ApiService
+    {
+        /// <summary>
+        /// ベースのURLを作成
+        /// </summary>
+        private readonly string baseUrl;
+        //private readonly string baseUrl = @"http://xxx.xxx.xxx.xxx:pp/api/";
+
+        /// <summary>
+        /// HttpClient このクラスのを使いまわす
+        /// </summary>
+        private readonly HttpClient httpClient;
+
+        private ApiService()
+        {
+
+        }
+
+        public ApiService(string baseUrl)
+        {
+            this.baseUrl = baseUrl;
+            this.httpClient = new HttpClient();
+        }
+
+        public string GetRequest(string actionName)
+        {
+            // リクエスト作成
+            String requestEndPoint = this.baseUrl + actionName;
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, requestEndPoint);
+
+            // レスポンスの箱を作る
+            string resBodyStr;
+            HttpStatusCode resStatusCode = HttpStatusCode.NotFound;
+            Task<HttpResponseMessage> response;
+            try
+            {
+                // 引数にrequestをとる場合はSendAsyncを使うそれ以外はGetAsyncやPostAsyncなどを使う
+                response = this.httpClient.SendAsync(request);
+                resBodyStr = response.Result.Content.ReadAsStringAsync().Result;    // 実際にリクエストを飛ばすのはこのタイミング
+                resStatusCode = response.Result.StatusCode;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+            return resBodyStr;
+        }
+
+
+
+    }
+}
